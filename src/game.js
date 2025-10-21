ae9521b2f19f775728f920298e44dac064705681
kynoloko <kynfestivo@gmail.com>
2025-10-21 10:47:26 -0500

    3_ agregamos diamente y animacion

--- src/game.js
+++ src/game.js
@@ -1,40 +1,106 @@
+
 export class Game extends Phaser.Scene {
 
   constructor() {
     super({ key: 'game' });
   }
 
+  init() {
+  this.score = 0;
+}
+
   preload() {
     this.load.image('background', './assets/fondo.png');
     this.load.image('animal', './assets/zorro.png');
     this.load.image('credito', './assets/final.png');
     this.load.image('nivel', './assets/piso.png');
+    this.load.image('pelota', './assets/bola.png');
+    //carga sprite
+    this.load.spritesheet('diamanteAzul','./assets/diamante.png',
+      { frameWidth: 48, frameHeight: 48 }
+);
 
   }
 
   create() {
+      this.physics.world.setBoundsCollision(true, true, true, false);
+
     this.add.image(500, 300, 'background');
     this.animalZorro = this.physics.add.image(400, 90, 'animal');
-     this.nivelNegro = this.physics.add.image(400, 90, 'nivel');
+    this.nivelNegro = this.physics.add.image(400, 510, 'nivel').setImmovable();
+    this.pelotaRebote = this.physics.add.image(400, 10, 'pelota');
+    this.pelotaRebote.setCollideWorldBounds(true);
+
+    this.add.sprite(40,40,'diamanteAzul');
+
+
+
 
     this.animalZorro.body.allowGravity=false;
+    this.nivelNegro.body.allowGravity=false;
+
      this.imagenFinal=this.add.image(400,180,'credito');
      this.imagenFinal.visible=false;
 
 
+    this.scoreText = this.add.text(16, 16, 'PUNTOS: 0', { 
+  fontSize: '20px', 
+  fill: '#fff', 
+  fontFamily: 'verdana, arial, sans-serif' 
+});
+
+    
+ //cambiar el rumbo de la pelota 
+ let velocity = 100 * Phaser.Math.Between(1.3, 2);
+if (Phaser.Math.Between(0, 10) > 5) {
+  velocity = 0 - velocity;
+}
+this.pelotaRebote.setVelocity(velocity, 10);
+
+
+this.physics.add.collider(this.pelotaRebote, this.nivelNegro ,this.impactoPlataform,null,this);
+this.pelotaRebote.setBounce(1);
 
      this.cursors=this.input.keyboard.createCursorKeys();
 
+
+//animacion
+
+
+
+
+
+
+
+
   }
+
+impactoPlataform() {
+    this.score++;
+   this.scoreText.setText('PUNTOS:' + this.score);
+}
+
+
 update(){
   if (this.cursors.left.isDown){
-    this.animalZorro.setVelocityX(-200);
+    this.nivelNegro.setVelocityX(-300);
   }
   else if(this.cursors.right.isDown){
-     this.animalZorro.setVelocityX(200);
+     this.nivelNegro.setVelocityX(300);
   }
   else {
-    this.animalZorro.setVelocity(0);
+    this.nivelNegro.setVelocity(0);
   }
+
+if (this.pelotaRebote.y > 600) {
+      console.log('fin');
+      this.imagenFinal.visible = true;
+      this.scene.pause();
+    }
+
+
+
+    
+
 }
 }
